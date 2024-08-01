52caf323ed7acd8560e5e98b4ad187c2c3a7abf0
exde17 <mabg0630@gmail.com>
2024-08-01 00:30:32 -0500

    actualiaCliente2

--- lib/src/utils/config.dart
+++ lib/src/utils/config.dart
@@ -1,4 +1,4 @@
 class GlobalConfig {
-  //static const String apiHost = 'http://3.138.126.32';
-  static const String apiHost = 'http://10.0.2.2';
+  static const String apiHost = 'http://3.138.126.32';
+  //static const String apiHost = 'http://10.0.2.2';
 }
